5ce7c7152b47a2bc9d7c44eaa77e1df9cead4493
itzkbp <hi@kailashpoudel.com.np>
2024-09-04 12:02:38 +0545

    change Navbar due to merge conflict

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import '../navbar/Navbar.css';
 import { useAuthGlobally } from '../../context/AuthContext';
+import toast from 'react-hot-toast';
+import axios from 'axios';
 import { useCartGlobally } from '../../context/CartContext';
-import { GiHamburgerMenu } from "react-icons/gi";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { FiShoppingCart } from "react-icons/fi";
 import { Badge } from '@mui/material';
 import Search from '../search/Search';
-import axios from 'axios';
-import toast from 'react-hot-toast';
-import './Navbar.css'; // Renamed to match conventional naming
 
 const Navbar = () => {
   const [auth, setAuth] = useAuthGlobally();
@@ -17,44 +16,61 @@ const Navbar = () => {
   const navigate = useNavigate();
 
   const handleLogout = () => {
-    setAuth({ user: null, token: null });
+    setAuth({
+      user: null,
+      token: null
+    });
     localStorage.removeItem('token');
-    delete axios.defaults.headers.common['Authorization'];
+    axios.defaults.headers.common['Authorization'] = null;
     toast.success('Logout Successfully');
     navigate('/login');
   };
 
   return (
-    <div className="navbar-container">
-      <div className="navbar">
-        <Link to="/" className="logo">
-          <img className="logo-img" src="/images/bio_life_Logo.png" alt="Bio Life Logo" />
-        </Link>
-        <div className="search-component">
-          <Search />
+    <div className="navbarContainer">
+      <div className="container">
+        <div className="navbar">
+
+          <div className="logo">
+            <Link to={'/'}><img className='logoImg' src="/images/bio_life_Logo.png" alt="Bio Life Logo" /></Link>
+          </div>
+
+          <div className='searchComponent'>
+            <Search />
+          </div>
+
+          <div>
+            <ul className='navlinks'>
+              <li><NavLink to="/">Home</NavLink></li>
+              <li><NavLink to="/products">Products</NavLink></li>
+              <li><NavLink to="/contact">Contact</NavLink></li>
+
+              <li><NavLink to="/blog">Blog</NavLink></li>
+              {
+                auth?.user ? (
+                  <div className="navbar-user">
+                    <li onClick={handleLogout}><NavLink to="#">Logout</NavLink></li>
+                  </div>
+                ) : (
+                  <>
+                    <li><NavLink to="/login">Login</NavLink></li>
+                  </>
+                )
+              }
+            </ul>
+          </div>
+          <NavLink to="/cart">
+            <Badge badgeContent={cart.length} color="error">
+              <div className='cart-icon'>
+                <FiShoppingCart />
+              </div>
+            </Badge>
+          </NavLink>
+          <p className='hamburgerMenu'><RxHamburgerMenu /></p>
         </div>
-        <ul className="navlinks">
-          <li><NavLink to="/">Home</NavLink></li>
-          <li><NavLink to="/products">Products</NavLink></li>
-          <li><NavLink to="/contact">Contact</NavLink></li>
-          <li>
-            <NavLink to="/cart">
-              <Badge badgeContent={cart.length} color="error">Cart</Badge>
-            </NavLink>
-          </li>
-          <li><NavLink to="/blog">Blog</NavLink></li>
-          {auth?.user ? (
-            <li onClick={handleLogout}><NavLink to="#">Logout</NavLink></li>
-          ) : (
-            <li><NavLink to="/login">Login</NavLink></li>
-          )}
-        </ul>
-        <button className="hamburger-menu">
-          <GiHamburgerMenu />
-        </button>
       </div>
     </div>
   );
-};
+}
 
 export default Navbar;
